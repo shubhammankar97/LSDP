9a1aa3f1e09949cd4839f75f168008f1e71b831f
AlexKutaloAlexey <AlexKutaloAlexey@github.com>
2022-07-11 19:32:04 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Home from './components/home/Home';
+import GetInvolved from './components/getinvolved/GetInvolved';
+import Faq from './components/faq/FAQ';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/involved" element={<GetInvolved />} />
+          <Route path="/faq" element={<Faq />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
